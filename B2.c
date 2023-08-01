240c68a15cd5c292b6503dd0234133646d9d8422
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-01 15:48:56 +0530

    Update B2.c

--- B2.c
+++ B2.c
@@ -2,14 +2,53 @@
 
 #include <stdio.h>
 #define MAX 20
-char stack[MAX];
+int stack[MAX];                                 //char used instead of int as this case is for a string (i.e. character array)
 int top;
-char ch;
+char ch;                                          //ch used to get each character of the string during reversing.
 void push(char ch);
 char pop();
-void display();
 
 int main() {
+  int i;
+  char string[30];
   top = -1;
-  char stack[MAX];
+  printf("Provide a String : ");
+  gets(string);
+  int n = strlen(string);                            //used to check length of the string entered.
+  for (i=0; i < n; i++)                               //pushing all character in the stack named string[]
+    {
+      ch = string[i];
+      push(ch);
+    }
+  for (i = 0; i < n; i++)                           //reverse as pop is LIFO
+  {
+    ch = pop();
+    string[i] = ch;
+  }
+  puts(string);                                   //string will be printed after all character poped.
+  return 0;
+}
+
+void push (char ch)
+{
+  if (top == MAX-1) {
+    printf("Overflow.\n");
+  }
+  else {
+    top = top + 1;
+    stack[top] = ch;
+  }
+}
+
+char pop()
+{
+  char item;
+  if (top == -1) {
+    printf("Underflow.\n");
+  }
+  else {
+    item = stack[top];
+    top = top - 1;
+    return (item);
+  }
 }
