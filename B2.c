087c0e42d18cce3d709a67acff102b2c2866a2a6
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-01 15:20:47 +0530

    Create B2.c

---
+++ B2.c
@@ -0,0 +1,15 @@
+//Write a program to reverse a String Using the Stack Data structure.
+
+#include <stdio.h>
+#define MAX 20
+char stack[MAX];
+int top;
+char ch;
+void push(char ch);
+char pop();
+void display();
+
+int main() {
+  top = -1;
+  char stack[MAX];
+}
