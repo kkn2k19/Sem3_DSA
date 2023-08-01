8ed98be1b9bfe675546dcdaea408d1cd1ae265da
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-01 07:47:24 +0530

    Create A2.c

---
+++ A2.c
@@ -0,0 +1,33 @@
+//Write a program  to display all unique elements of an array. 
+
+#include <stdio.h>
+int unique(int array[], int n);                    //unique - it is the function created to check for unique elements in the array.
+
+int main()
+{
+   int arr[30], n, i;
+   printf("Enter size of Array : ");
+   scanf("%d",&n);
+   printf("Enter the array elements : ");
+   for(i = 0; i < n; i++){
+      scanf("%d", &arr[i]);
+   }
+   unique(arr, n);
+   return 0;
+}
+
+int unique(int arr[], int n){
+   int i, j, c=0;
+  printf("unique elements in an array is : ");
+   for(i = 0; i < n; i++){
+      for(j = 0; j < n; j++){
+         if(arr[i] == arr[j] && i != j)
+         break;
+      }
+      if(j == n ){
+         printf("%d ", arr[i]);
+         c++;
+      }
+   }
+   return -1;
+}
