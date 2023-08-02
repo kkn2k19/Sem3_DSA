c1d896e2d9ba76a2495bc00db6158221bf3988a9
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-02 20:18:07 +0530

    Create F3.c

---
+++ F3.c
@@ -0,0 +1,20 @@
+/*
+Print the Fibonacci Series up to nth term
+FIB(n) = 0, if n = 0
+       = 1, if n = 1
+       = FIB(n-1) + FIB(n-2), Otherwise
+*/
+
+#include <stdio.h>
+int fib(int n);
+
+int main () 
+{
+  int n, k;
+  printf("Enter no. of terms : ");
+  scanf("%d", &n);
+  if (n == 0) {
+    ret
+  }
+  return 0;
+}
