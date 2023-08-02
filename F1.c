f939c9570766aea1146366b6baa3890ce4945abf
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-02 20:04:38 +0530

    Create F1.c

---
+++ F1.c
@@ -0,0 +1,29 @@
+/*Find the GCD of two numbers recursively by Euclids algorithm which states 
+
+      GCD (a,b) = b       if b divides a
+                = GCD (b, a mod b), otherwise 
+*/
+
+#include <stdio.h>
+int gcd (int a, int b);
+
+int main () 
+{
+  int a, b, res;
+  printf("Provide two numbers : ");
+  scanf ("%d %d", &a, &b);
+  res = gcd(a, b);
+  printf("GCD : %d\n", res);
+}
+
+int gcd (int a, int b) {
+  int r, k;
+  r = a%b;
+  if (r == 0) {
+    return (b);
+  }
+  else {
+    k = GCD (b, r);
+    return (k);
+  }
+}
