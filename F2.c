60a773809f2a8f713def256ab36725ee898d9e32
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-04 16:10:59 +0530

    Update F2.c

--- F2.c
+++ F2.c
@@ -4,4 +4,28 @@ Find X^y
               = X*EXP(X, Y-1), otherwise
 */
 
-#
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float k;
+    int x;
+    int y;
+    printf("Enter no. and its exponential power : ");
+    scanf("%d %d", &x, &y);
+    k = expo(x, y);
+    printf("Result : %f\n", k);
+    return 0;
+}
+
+int expo(int x, int y){
+    float k;
+    if (y == 0) {
+            return 1.0;
+    }
+    else {
+            k = x*expo(x, y-1);
+            return k;
+    }
+}
