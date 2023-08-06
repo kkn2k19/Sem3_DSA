895a43583ee46b52775c58580c47e4b8d02ab9a3
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 21:52:32 +0530

    Create E.c

---
+++ E.c
@@ -0,0 +1,14 @@
+/*
+The famous car manufacturing compary, NavaVista, launching their Battery Operated subsidized 6-seater Car this 1st December. 
+For that NavaVista had opened the booking for 100 customers only. 
+Your job is to maintain the booking details on a First book First get basis. 
+You have to accept the foolowing details from the customers : (Name, AadharNo, MobileNo, City). 
+Write a menu-driven program to accept a booking, show the customers details, and sequence in which booking will be served on 1st December.
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+  
+}
