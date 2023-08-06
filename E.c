7b8e254582941f00a88d9dca127fe9c9d8004c93
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-06 21:57:19 +0530

    Update E.c

--- E.c
+++ E.c
@@ -6,9 +6,6 @@ You have to accept the foolowing details from the customers : (Name, AadharNo, M
 Write a menu-driven program to accept a booking, show the customers details, and sequence in which booking will be served on 1st December.
 */
 
-#include <stdio.h>
+NOT COMPLETE YET !!!!!
 
-int main () 
-{
-  
-}
+  THIS PORTION IS BASED ON QUEUE USING STRUCTURE I HAVE NOT REACHED THERE TILL NOW...
