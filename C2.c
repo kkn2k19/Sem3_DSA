d14b37a6a56533f6a814f403364bbc044244cde2
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-10 23:45:05 +0530

    Update C2.c

--- C2.c
+++ C2.c
@@ -1,3 +1,28 @@
 //Write a program to convert Infix Expression to Postfix Expression.
 
-NOT DONE!!!!
+NOT COMPLETE!!!!!
+  
+#include <stdio.h>
+#define MAX 10
+char stack[MAX];
+int top;
+void push (char ch);
+char pop ();
+void display();
+void convertion(char exp[]);
+
+int main () 
+{
+  top = -1;
+  char exp[20];
+  printf("Enter infix expression : ");
+  gets(exp);
+  convertion(infix);
+  return 0;
+}
+
+void convertion (char exp[])
+{
+  
+}
+void push ()
