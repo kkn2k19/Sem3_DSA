cfd1848eec27eb1924f47f81d1e07f262a37015a
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-16 01:14:05 +0530

    Update G1.c

--- G1.c
+++ G1.c
@@ -10,130 +10,266 @@ e. Reversing the list
 f. Deletion of a particular node
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 struct box 
 {
-  int data;
-  int box*next;
+    int data;
+    struct box *next;
 };
 
 typedef struct box node;
 
-node*head;
-node*last_node;
+node *head = NULL;
+node *last_node = NULL;
 
-int main () 
+void insertion();
+void traverse();
+int searching();
+void insert_after(int se);
+void insert_before(int se);
+void reverse_list();
+void delete_node(int se);
+
+int main() 
 {
-  head = NULL;
-  last_node = NULL;
-  int option;
-  while (option != 4) {
-    printf("Enter 1 for insertion,\n2 for traversing,\n3 for searching,\n4 for Exiting. : ");
-    scanf("%d", &option);
-    switch(option) {
-      case 1 :
-             insertion();
-             break;
-      case 2 :
-             traversing();
-             break;
-      case 3 : 
-             searching();
-             break;
-      case 4 : 
-             printf("Exiting.\n");
-      default:
-             printf("Give input from 1 to 4.\n");
+    int option, value;
+
+    do
+    {
+        printf("\nMenu:\n");
+        printf("1. Creation\n");
+        printf("2. Insertion\n");
+        printf("3. Insert after a specific node\n");
+        printf("4. Insert before a specific node\n");
+        printf("5. Traversing\n");
+        printf("6. Searching\n");
+        printf("7. Reversing the list\n");
+        printf("8. Deletion of a particular node\n");
+        printf("9. Exit\n");
+        printf("Enter your option: ");
+        scanf("%d", &option);
+
+        switch(option) 
+        {
+            case 1:
+                insertion();
+                break;
+            case 2:
+                insertion();
+                break;
+            case 3: 
+                printf("Enter the value after which to insert: ");
+                scanf("%d", &value);
+                insert_after(value);
+                break;
+            case 4: 
+                printf("Enter the value before which to insert: ");
+                scanf("%d", &value);
+                insert_before(value);
+                break;
+            case 5:
+                traverse();
+                break;
+            case 6: 
+                printf("Enter the number to search: ");
+                if(searching())
+                    printf("Found\n");
+                else
+                    printf("Not Found\n");
+                break;
+            case 7:
+                reverse_list();
+                break;
+            case 8:
+                printf("Enter the value of the node to delete: ");
+                scanf("%d", &value);
+                delete_node(value);
+                break;
+            case 9:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid option. Please choose a number between 1 and 9.\n");
+        }
+    } while(option != 9);
+
+    return 0;
+}
+
+void insertion() 
+{
+    node *temp = (node*)malloc(sizeof(node));
+    if (!temp) 
+    {
+        printf("Memory allocation failed!\n");
+        return;
     }
-  }
-  return 0;
+    printf("Input data: ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    if (!head) 
+    {
+        head = temp;
+        last_node = temp;
+    } 
+    else 
+    {
+        last_node->next = temp;
+        last_node = temp;
+    }
 }
 
-void insertion () {
-  node*temp;
-  temp = (node*)malloc(sizeof(node));
-  printf("Input data : ");
-  scanf("%d", &temp -> data);
-   temp -> next = NULL;
-   if (head == NULL) {
-      head = temp;
-      last_node = temp;
-   }
-   else {
-      last_node -> next = temp;
-      last_node = temp;
-   }
+void traverse() 
+{
+    node *dh = head;
+    while (dh) 
+    {
+        printf("%d ", dh->data);
+        dh = dh->next;
+    }
+    printf("\n");
 }
 
-void traverse () {
-   node *dh = head;
-   while (dh != NULL) {
-      printf("%d\n", dh->data);
-      dh = dh -> next;
-   }
+int searching() 
+{
+    int se;
+    int flag = 0;
+    node *dh = head;
+    scanf("%d", &se);
+    while (dh) 
+    {
+        if (se == dh->data) 
+        {
+            flag = 1;
+            break;
+        }
+        dh = dh->next;
+    }
+    return flag;
 }
 
-int searching () {
-   int se;
-   int flag = 0;
-   node *dh = head;
-   scanf("%d", &se);
-   while (dh != NULL) {
-      if (se == dh -> data) {
-         flag = 1;
-         break;
-      }
-      dh = dh -> next;
-   }
-   return (flag);
+void insert_after(int se) 
+{
+    node *dh = head;
+    node *temp;
+    while (dh) 
+    {
+        if (se == dh->data) 
+            break;
+        dh = dh->next;
+    }
+    if (!dh) 
+    {
+        printf("Not Found: %d\n", se);
+        return;
+    }
+    temp = (node *)malloc(sizeof(node));
+    if (!temp) 
+    {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+    printf("Enter data to insert after %d: ", se);
+    scanf("%d", &temp->data);
+    temp->next = dh->next;
+    dh->next = temp;
+    if (!temp->next) 
+        last_node = temp;
 }
 
-void insert_after (int se) {
-   node *dh = head;
-   node *temp;
-   int flag = 0;
-   while (dh != NULL) {
-      if (se = dh -> data) {
-         flag = 1;
-         break;
-      }
-      dh = dh -> next;
-   }
-   if (flag == 0) {
-      printf("Not Found : %d\n", se);
-      return;
-   }
-   temp = (node *)malloc(sizeof(node));
-   scanf("%d", &temp -> data);
-   temp -> next = dh -> next;
-   dh -> next = temp;
-   if (temp -> next == NULL) {
-      last_node = temp;
-   }
+void insert_before(int se) 
+{
+    if (!head) 
+    {
+        printf("List is empty.\n");
+        return;
+    }
+    
+    if (head->data == se) 
+    {
+        node *temp = (node *)malloc(sizeof(node));
+        if (!temp) 
+        {
+            printf("Memory allocation failed!\n");
+            return;
+        }
+        printf("Enter data to insert before %d: ", se);
+        scanf("%d", &temp->data);
+        temp->next = head;
+        head = temp;
+        return;
+    }
+
+    node *dh = head;
+    node *temp;
+    while (dh->next && dh->next->data != se) 
+        dh = dh->next;
+
+    if (!dh->next) 
+    {
+        printf("Not Found: %d\n", se);
+        return;
+    }
+    
+    temp = (node *)malloc(sizeof(node));
+    if (!temp) 
+    {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+    printf("Enter data to insert before %d: ", se);
+    scanf("%d", &temp->data);
+    temp->next = dh->next;
+    dh->next = temp;
 }
 
-void insert_before (int se) {
-   node *dh = head;
-   if (head -> data == se) {
-      temp = (node *)malloc(sizeof(node));
-      scanf("%d", &temp -> data);
-      temp -> next = head;
-      head = temp;
-      return;
-   }
-   flag = 0;
-   while (dh -> next != NULL) {
-      if (dh -> next -> data == se) {
-         flag = 1;
-         break;
-      }
-      dh = dh -> next;
-   }
-   if (flag == 0) {
-      printf("Not Found.\n");
-      return;
-   }
-   temp = (node *)malloc(sizeof(node));
-   scanf("%d", &temp -> data);
-   temp -> next = dh -> next;
-   dh -> next = temp;
+void reverse_list() 
+{
+    node *prev = NULL, *current = head, *next = NULL;
+    while (current) 
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+}
+
+void delete_node(int se) 
+{
+    node *temp, *prev;
+    if (!head) 
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    if (head->data == se) 
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+        return;
+    }
+
+    temp = head;
+    while (temp && temp->data != se) 
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (!temp) 
+    {
+        printf("Node not found: %d\n", se);
+        return;
+    }
+
+    prev->next = temp->next;
+    if (!temp->next) 
+        last_node = prev;
+    free(temp);
 }
