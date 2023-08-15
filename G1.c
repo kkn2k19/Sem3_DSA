050372ac65449b8171da3c2db82d1fa431b2b99c
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-16 00:32:17 +0530

    Create G1.c

---
+++ G1.c
@@ -0,0 +1,12 @@
+/*
+Write a menu-driven program to implement the Single Link List and perform the following operations.
+a. Creation
+b. Insertion
+   i.  After a Specific Node 
+   ii. Before a Specific Node 
+c. Searching
+d. Traversing
+e. Reversing the list
+f. Deletion of a particular node
+*/
+
