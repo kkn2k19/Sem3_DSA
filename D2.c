a7f94749756e2f0cc22bf94cfc8300b61cc0f018
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-16 01:16:31 +0530

    Update D2.c

--- D2.c
+++ D2.c
@@ -6,78 +6,109 @@ NOT COMPLETE YET!!!
 
 #include <stdio.h>
 #define MAX 10
+
 int CQ[MAX];
-int R, F, flag;
-void insertion (int item);
-int deletion ();
-void display ();
+int R, F;
 
-int main ()
+void insertion(int item);
+int deletion();
+void display();
+
+int main()
 {
-  int option;
-  int item;
-  int k;
-  F = MAX-1;
-  R = MAX-1;
-  flag = 0;
-  while (option != 4) {
-    printf("Press 1 for insertion,\n2 for deletion,\n3 for display,\n4 for exit.\n");
-    scanf("%d", &option);
-    switch(option) {
-      case 1 :
-             printf("Enter number to insert : ");
-             scanf("%d", &item);
-             insertion (item);
-             break;
-      case 2 :
-             k = deletion();
-             printf("Deleted item : %d\n", k);
-             break;
-      case 3 :
-             display ();
-             break;
-      case 4 :
-             printf("Exiting.\n");
-    }
-  }
-  return 0;
+    int option;
+    int item;
+    int k;
+    
+    F = -1;
+    R = -1;
+    
+    do
+    {
+        printf("Press 1 for insertion,\n2 for deletion,\n3 for display,\n4 for exit.\n");
+        scanf("%d", &option);
+        
+        switch(option)
+        {
+            case 1:
+                printf("Enter number to insert: ");
+                scanf("%d", &item);
+                insertion(item);
+                break;
+            case 2:
+                k = deletion();
+                if (k != -1)
+                    printf("Deleted item: %d\n", k);
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("Exiting.\n");
+                break;
+            default:
+                printf("Invalid option. Please choose between 1-4.\n");
+        }
+    } while(option != 4);
+
+    return 0;
 }
 
-void insertion (int item) {
-  if (F == R && flag == 1) {
-    printf("Overflow.\n");
-  }
-  else {
-    R = (R+1)%MAX;
+void insertion(int item)
+{
+    if ((R + 1) % MAX == F)
+    {
+        printf("Overflow.\n");
+        return;
+    }
+    
+    if (F == -1) F = 0;
+    
+    R = (R + 1) % MAX;
     CQ[R] = item;
-    flag = 1;
-  }
 }
 
-int deletion () {
-  int item;
-  if (F == R && flag == 0) {
-    printf("Underflow.\n");
-  }
-  else {
-    F = (F+1)%MAX;
-    item = CQ[F];
-    flag = 0;
-    F = F+1;
-    return (item);
-  }
-}
-
-void display () {
-  int i;
-  if (F != -1) {
-    printf("Circular Queue.\n");
-    for (i = F; i != R; i++) {
-      printf("%d ", CQ[i]);
+int deletion()
+{
+    if (F == -1)
+    {
+        printf("Underflow.\n");
+        return -1;
     }
-    printf("%d ", CQ[i]);
-  }
-  else {
-    printf("Circular Queue is empty.\n");
-  }
+    
+    int item = CQ[F];
+    
+    if (F == R)
+    {
+        F = R = -1;
+    }
+    else
+    {
+        F = (F + 1) % MAX;
+    }
+
+    return item;
+}
+
+void display()
+{
+    if (F == -1)
+    {
+        printf("Circular Queue is empty.\n");
+        return;
+    }
+
+    printf("Circular Queue: ");
+    int i = F;
+
+    while(1)
+    {
+        printf("%d ", CQ[i]);
+        
+        if (i == R) break;
+        
+        i = (i + 1) % MAX;
+    }
+
+    printf("\n");
 }
