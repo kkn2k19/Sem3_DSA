9a6f910265b3af718346f94b79c6feca55710766
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-19 13:06:34 +0530

    Update C2.c

--- C2.c
+++ C2.c
@@ -1,11 +1,8 @@
-//Write a program to convert Infix Expression to Postfix Expression.
-
-NOT COMPLETE!!!!
+// Write a program to convert Infix Expression to Postfix Expression.
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> 
-#include <ctype.h>
+#include <stdlib.h>
 #define MAX 30
 
 char stack[MAX];
@@ -15,119 +12,100 @@ void push(char ch);
 char pop();
 int operator_check(char ch);
 int operator_precedence(char ch);
-void convertion(char infix[], char postfix[]);
+void conversion(char infix[], char postfix[]);
 
 int main()
 {
-  top = -1;
-  char infix[20];
-  printf("Enter infix expression: ");
-  fgets(infix, sizeof(infix), stdin);
-  char postfix[20];
-  convertion(infix, postfix);
-  printf("Postfix Expression: %s\n", postfix);
-  return 0;
+    top = -1;
+    char infix[20];
+    printf("Provide Infix Expression : ");
+    fgets(infix, sizeof(infix), stdin);
+    size_t len = strlen(infix);
+    if(len > 0 && infix[len - 1] == '\n')
+        infix[len - 1] = '\0';
+    char postfix[20];
+    conversion (infix, postfix);
+    printf("Postfix Expression : %s\n", postfix);
+    return 0;
 }
 
-int operator_check(char ch)
-{
-  switch(ch)
-  {
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-    case '^':
-      return 1;
-    default:
-      return 0;
-  }
+int operator_check(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
 }
 
-int operator_precedence(char ch)
-{
-  switch(ch)
-  {
-    case '^':
-      return 3;
-    case '*':
-    case '/':
-      return 2;
-    case '+':
-    case '-':
-      return 1;
-    default:
-      return 0;
-  }
+int operator_precedence(char ch) {
+    if (ch == '*' || ch == '/')
+        {
+            return 2;
+        }
+    else if (ch == '+' || ch == '-')
+        {
+            return 1;
+        }
+    return 0;
 }
 
-void convertion(char infix[], char postfix[])
+void conversion(char infix[], char postfix[])
 {
-  int i, j;
-  char item;
-  char x;
-  
-  push('(');
-  strcat(infix, ")");
-  
-  i = 0;
-  j = 0;
-  item = infix[i];
-  
-  while(item != '\0')
-  {
-    if(item == '(')
-      push(item);
-    else if(isalnum(item))  // Alphanumeric characters
-    {
-      postfix[j++] = item;
+    int i = 0;
+    int j = 0;
+    char k;
+    char ch;
+
+    push('(');
+    strcat(infix, ")");
+
+    ch = infix[i];
+    while (ch != '\0') {
+        if (ch == '(') {
+            push(ch);
+        }
+        else if ((ch >= '0' && ch <= '9')||(ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            postfix[j++] = ch;
+        }
+        else if (operator_check(ch) == 1) {
+            k = pop();
+            while(operator_check(k) && operator_precedence(k) >= operator_precedence(ch)) {
+                postfix[j++] = k;
+                k = pop();
+            }
+            push(k);
+            push(ch);
+        }
+        else if (ch == ')') {
+            k = pop();
+            while (k != '(') {
+                postfix[j++] = k;
+                k = pop();
+            }
+        }
+        else {
+            printf("Invalid Expression.\n");
+            return;
+        }
+        ch = infix[++i];
     }
-    else if(operator_check(item) == 1)
-    {
-      x = pop();
-      while(operator_check(x) && operator_precedence(x) >= operator_precedence(item))
-      {
-        postfix[j++] = x;
-        x = pop();
-      }
-      push(x);  // Push back the last popped item which does not satisfy the condition.
-      push(item);
-    }
-    else if(item == ')')
-    {
-      x = pop();
-      while(x != '(')
-      {
-        postfix[j++] = x;
-        x = pop();
-      }
-    }
-    else
-    {
-      printf("Invalid expression.\n");
-      return;
-    }
-    i++;
-    item = infix[i];
-  }
-  postfix[j] = '\0';
+    postfix[j] = '\0';
 }
 
-void push(char ch)
-{
-  if(top == MAX-1)
-    printf("Overflow.\n");
-  else
-    stack[++top] = ch; 
+void push (char ch) {
+    if (top == MAX-1) {
+        printf("Overflow.\n");
+    }
+    else {
+        top = top +1;
+        stack[top] = ch;
+    }
 }
 
-char pop()
-{
-  if(top == -1)
-  {
-    printf("Underflow.\n");
-    return '\0';  // Return a null character to indicate underflow
-  }
-  else
-    return stack[top--];
+char pop() {
+    char ch = ' ';
+    if (top == -1){
+        printf("Underflow.\n");
+    }
+    else {
+        ch = stack[top];
+        top = top-1;
+    }
+    return ch;
 }
