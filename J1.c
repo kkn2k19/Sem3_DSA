6682b4c72f4a6c7fbbafce2ea4014e0ca2cab72c
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-09-01 23:02:21 +0530

    Create J1.c

---
+++ J1.c
@@ -0,0 +1,21 @@
+/*
+1. Write a program to implement Binary Search Tree and do the following operations.
+   a. Creation
+   b. Insertion
+   c. Traversing (IN, PRE, POST Order)
+   d. Searching
+*/
+
+NOT COMPLETE!!!!!
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tree {
+  int data;
+  struct tree* left;
+  struct tree* right;
+};
+
+typedef struct tree node;
+
